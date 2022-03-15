14107dbdcc1cb67dfc91794c5773103240e90274
Eloise Gagne <70410591+Paracoco@users.noreply.github.com>
2022-03-14 23:03:34 -0400

    carousel test

--- scripts/billboard.js
+++ scripts/billboard.js
@@ -5,10 +5,10 @@ myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })*/
 
-const swiper = new swiper('.mySwiper', {
+/*const swiper = new swiper('.mySwiper', {
   speed: 400,
   spaceBetween: 100,
   direction:'horizontal',
   effect:'slide',
   
-});
+});*/
