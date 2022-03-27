b76ada64819ac0e180b095207d118d51425a799e
Eloise Gagne <70410591+Paracoco@users.noreply.github.com>
2022-03-26 20:04:09 -0400

    scrolltrigger

--- scripts/home.js
+++ scripts/home.js
@@ -3,3 +3,14 @@
         el: ".swiper-pagination",
     },
 });*/
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.timeline', {
+    scrollTrigger: {
+    markers: true,
+    trigger: '.timeline',
+    },
+    x: '100%',
+    duration: 2,
+});
