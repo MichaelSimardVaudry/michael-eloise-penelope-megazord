84d1da8122e291c646adcffe28d3a6af2450a0fe
MichaelSimardVaudry <78884924+MichaelSimardVaudry@users.noreply.github.com>
2022-03-30 22:38:14 -0400

    hum yea :hospital:  :blowfish:

--- scripts/discography.js
+++ scripts/discography.js
@@ -1,14 +1,150 @@
 gsap.registerPlugin(ScrollTrigger);
+gsap.from(".n01", {
+  scrollTrigger: {
+    start: "top 50%",
+    end: "bottom 50%",
 
-gsap
-  .to(".animation", {
-    duration: 2,
-    scrollTrigger: {
-      markers: true,
-      start: "top 75%",
-      end: "bottom 25%",
-      toggleActions: "restart complete reverse reset",
-      trigger: ".animation",
-    },
-  })
-  .to(".element.no2", { x: "100%" });
+    trigger: ".n01",
+    toggleActions: "restart complete reverse reset",
+  },
+  x: "100%",
+  duration: 2,
+});
+gsap.from(".savoir01", {
+  scrollTrigger: {
+    start: "top 50%",
+    end: "bottom 50%",
+    trigger: ".n01",
+    toggleActions: "restart complete reverse reset",
+  },
+  rotate: "360",
+  duration: 2,
+});
+gsap.from(".n02", {
+  scrollTrigger: {
+    start: "top 50%",
+    end: "bottom 50%",
+
+    trigger: ".n02",
+    toggleActions: "restart complete reverse reset",
+  },
+  x: "100%",
+  duration: 2,
+});
+gsap.from(".savoir02", {
+  scrollTrigger: {
+    start: "top 50%",
+    end: "bottom 50%",
+    trigger: ".n02",
+    toggleActions: "restart complete reverse reset",
+  },
+  rotate: "-360",
+  duration: 2,
+});
+gsap.from(".n03", {
+  scrollTrigger: {
+    start: "top 50%",
+    end: "bottom 50%",
+
+    trigger: ".n03",
+    toggleActions: "restart complete reverse reset",
+  },
+  x: "100%",
+  duration: 2,
+});
+gsap.from(".savoir03", {
+  scrollTrigger: {
+    start: "top 50%",
+    end: "bottom 50%",
+    trigger: ".n03",
+    toggleActions: "restart complete reverse reset",
+  },
+  rotate: "360",
+  duration: 2,
+});
+gsap.from(".n04", {
+  scrollTrigger: {
+    start: "top 50%",
+    end: "bottom 50%",
+
+    trigger: ".n04",
+    toggleActions: "restart complete reverse reset",
+  },
+  x: "100%",
+  duration: 2,
+});
+gsap.from(".savoir04", {
+  scrollTrigger: {
+    start: "top 50%",
+    end: "bottom 50%",
+    trigger: ".n04",
+    toggleActions: "restart complete reverse reset",
+  },
+  rotate: "-360",
+  duration: 2,
+});
+gsap.from(".n05", {
+  scrollTrigger: {
+    start: "top 50%",
+    end: "bottom 50%",
+
+    trigger: ".n05",
+    toggleActions: "restart complete reverse reset",
+  },
+  x: "100%",
+  duration: 2,
+});
+gsap.from(".savoir05", {
+  scrollTrigger: {
+    start: "top 50%",
+    end: "bottom 50%",
+    trigger: ".n05",
+    toggleActions: "restart complete reverse reset",
+  },
+  rotate: "360",
+  duration: 2,
+});
+
+gsap.from(".n06", {
+  scrollTrigger: {
+    start: "top 50%",
+    end: "bottom 50%",
+
+    trigger: ".n06",
+    toggleActions: "restart complete reverse reset",
+  },
+  x: "100%",
+  duration: 2,
+});
+gsap.from(".savoir06", {
+  scrollTrigger: {
+    start: "top 50%",
+    end: "bottom 50%",
+    trigger: ".n06",
+    toggleActions: "restart complete reverse reset",
+  },
+  rotate: "-360",
+  duration: 2,
+});
+gsap.from(".n07", {
+  scrollTrigger: {
+    start: "top 50%",
+    end: "bottom 50%",
+
+    trigger: ".n07",
+    toggleActions: "restart complete reverse reset",
+  },
+  x: "100%",
+  duration: 2,
+});
+gsap.from(".animTitle", {
+  scrollTrigger: {
+    start: "top 50%",
+    end: "bottom 50%",
+
+    trigger: ".n07",
+    toggleActions: "restart complete reverse reset",
+  },
+  rotate: "360",
+  duration: 2,
+});
