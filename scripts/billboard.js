94996cad3b082ad5b2139b4431905b9c45897b64
Penelope-M <penelopemorrisson1999@gmail.com>
2022-04-07 13:24:51 -0400

    :accept:  fonctionne

--- scripts/billboard.js
+++ scripts/billboard.js
@@ -23,3 +23,38 @@ gsap.to('.no1', {
   rotation: '360%',
   duration: 2,
 });
+
+
+
+const parapuie = document.querySelector('.parapuie');
+const main = document.querySelector('#main');
+let temps;
+
+gsap.to('#main', {
+  scrollTrigger: {
+    trigger: '#main',
+    scrub: 0.25,
+    onUpdate: (e) => {
+      parapuie.classList.add("is-scrolling");
+    }
+  }
+});
+gsap.to(".parapuie", {
+  scrollTrigger: {
+    scrub: true,
+    markers: true,
+    start: "top 0%",
+    end: "bottom 100%",
+    trigger: "#main",
+    onUpdate: (e) => {
+      if (e.direction == 1) {
+        parapuie.classList.remove("haut");
+        parapuie.classList.add("bas");
+        
+      } else {
+        parapuie.classList.remove("bas");
+        parapuie.classList.add("haut");
+      }
+    }
+  }
+});
